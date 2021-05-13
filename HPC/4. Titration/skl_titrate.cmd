d1048894a5fd31d01bd3a8482436a178d1fd013d
Han <jungmin.s.han@gmail.com>
2021-05-13 12:27:02 -0400

    updated make-swarm

--- HPC/4. Titration/skl_titrate.cmd
+++ HPC/4. Titration/skl_titrate.cmd
@@ -1,8 +1,2 @@
 python3 skl_titrate_l2.py 10
 python3 skl_titrate_l2.py 20
-python3 skl_titrate_l2.py 30
-python3 skl_titrate_l2.py 40
-python3 skl_titrate_l2.py 50
-python3 skl_titrate_l2.py 60
-python3 skl_titrate_l2.py 70
-python3 skl_titrate_l2.py 80
